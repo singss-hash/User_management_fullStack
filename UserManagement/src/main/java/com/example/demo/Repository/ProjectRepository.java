c849882b1dcc28c1190bfd523728dc17e0976d93
singss-hash <singssp03ragya@gmail.com>
2024-10-16 12:24:42 +0530

    Delete UserManagement directory

--- UserManagement/src/main/java/com/example/demo/Repository/ProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-	 boolean existsByProjectId(Long projectId);
-}
